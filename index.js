f9a6cbc39607f8828ffde7b13737aaf29ae53f67
Barbora Ševčíková <bsevcikova@ABOUT.LOCAL>
2022-11-21 17:54:03 +0100

    hearbeat

--- index.js
+++ index.js
@@ -13,63 +13,48 @@ document.getElementById('btn-modal').addEventListener('click', function() {
     document.getElementById('overlay').classList.remove('is-visible');
     document.getElementById('modal').classList.remove('is-visible');
   });
-
-
-
-
 // Get DOM Elements
 const modal = document.querySelector('#my-modal');
 const modalBtn = document.querySelector('#modal-btn');
 const closeBtn = document.querySelector('.close');
-
 // Events
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick);
-
 // Open
 function openModal() {
   modal.style.display = 'block';
 }
-
 // Close
 function closeModal() {
   modal.style.display = 'none';
 }
-
 // Close If Outside Click
 function outsideClick(e) {
   if (e.target == modal) {
     modal.style.display = 'none';
   }
 }
-
 // Get the modal
 var ebModal = document.getElementById('mySizeChartModal');
-
 // Get the button that opens the modal
 var ebBtn = document.getElementById("mySizeChart");
-
 // Get the <span> element that closes the modal
 var ebSpan = document.getElementsByClassName("ebcf_close")[0];
-
 // When the user clicks the button, open the modal 
 ebBtn.onclick = function() {
     ebModal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 ebSpan.onclick = function() {
     ebModal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == ebModal) {
         ebModal.style.display = "none";
     }
 }
-
 */
 
 
@@ -208,7 +193,3 @@ function increaseCount(){
       $(this).addClass("disabled"); // For visual clue that button no longer works
  }
 
-
-
- 
- 
