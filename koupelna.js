b5f7ae88b526ddfa879411ec296e97723719bcf7
Barbora Ševčíková <bsevcikova@ABOUT.LOCAL>
2022-11-28 11:13:58 +0100

    fen

--- koupelna.js
+++ koupelna.js
@@ -35,25 +35,96 @@ window.onclick = function(event) {
 
 
 
-//Modaly Ložnice
-// Kočka 
-var ebModal = document.getElementById('koupelna--modal_karma');
-var ebBtn = document.getElementById("koupelna--link_karma");
-var ebSpan = document.getElementsByClassName("koupelna--modal")[0];
+//Modaly Koupelna
+// Karma
+var ebModal1 = document.getElementById('koupelna--modal_karma');
 
+// Get the button that opens the modal
+var ebBtn1 = document.getElementById("koupelna--link_karma");
 
-ebBtn.onclick = function() {
-    ebModal.style.display = "block";
+// Get the <span> element that closes the modal
+var ebSpan1 = document.getElementsByClassName("koupelna--modal_karma")[0];
+
+// When the user clicks the button, open the modal 
+ebBtn1.onclick = function() {
+    ebModal1.style.display = "block";
 }
 
-
-ebSpan.onclick = function() {
-    ebModal.style.display = "none";
+// When the user clicks on <span> (x), close the modal
+ebSpan1.onclick = function() {
+    ebModal1.style.display = "none";
 }
 
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == ebModal) {
-        ebModal.style.display = "none";
+    if (event.target == ebModal1) {
+        ebModal1.style.display = "none";
     }
 }
 
+// Vana
+var ebModal2 = document.getElementById('koupelna--modal_vana');
+var ebBtn2 = document.getElementById("koupelna--link_vana");
+var ebSpan2 = document.getElementsByClassName("koupelna--modal_vana")[0];
+
+
+ebBtn2.onclick = function() {
+    ebModal2.style.display = "block";
+}
+ebSpan2.onclick = function() {
+    ebModal2.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == ebModal2) {
+        ebModal2.style.display = "none";
+    }
+}
+
+// Plisen
+var ebModal3 = document.getElementById('koupelna--modal_plisen');
+var ebBtn3 = document.getElementById("koupelna--link_plisen");
+var ebSpan3 = document.getElementsByClassName("koupelna--modal_plisen")[0];
+
+
+ebBtn3.onclick = function() {
+    ebModal3.style.display = "block";
+}
+ebSpan3.onclick = function() {
+    ebModal3.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == ebModal3) {
+        ebModal3.style.display = "none";
+    }
+}
+
+
+
+/*počítání*/
+
+
+var clicks = 0
+function clickME(event) {
+  clicks += 1
+
+  document.getElementById("clicks").innerText = clicks // innerText is more suitable in this case
+
+  console.log(clicks)
+
+  if(clicks == 3) {
+   document.getElementById('gohome').style.display = 'block'; 
+   document.getElementById('zmiz').style.display = 'none';
+   document.getElementById('koupelna-hint').style.display = 'none';
+   localStorage.setItem("mistnost", "loznice")
+
+} else {
+   document.getElementById('gohome').style.display = 'none';
+}
+
+
+}
+
+document.querySelectorAll(`a`) // select all buttons
+  .forEach( 
+    el => el.addEventListener(`click`, clickME, {once: true})
+  ) 
